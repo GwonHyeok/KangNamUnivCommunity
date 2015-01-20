bebc700d271d5e6e3fe8a267be7ed6472b4e9532
GwonHyeok <kh4975@naver.com>
2015-01-20 17:02:03 +0900

    GCM UPDATE         1. make photo image usable         2. show Notification with title, message

--- app/src/main/java/com/yscn/knucommunity/GCM/GcmIntentService.java
+++ app/src/main/java/com/yscn/knucommunity/GCM/GcmIntentService.java
@@ -5,14 +5,16 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.yscn.knucommunity.Activity.Splash;
 import com.yscn.knucommunity.R;
+import com.yscn.knucommunity.Util.ImageLoaderUtil;
 
 /**
  * Created by GwonHyeok on 15. 1. 17..
@@ -43,50 +45,52 @@ public class GcmIntentService extends IntentService {
              */
             if (GoogleCloudMessaging.
                     MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
+//                sendNotification("Send error: " + extras.toString());
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " +
-                        extras.toString());
+//                sendNotification("Deleted messages on server: " +
+//                        extras.toString());
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Log.i(getClass().getSimpleName(), "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.i(getClass().getSimpleName(), "Completed work @ " + SystemClock.elapsedRealtime());
-                // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
+                sendNotification(extras);
                 Log.i(getClass().getSimpleName(), "Received: " + extras.toString());
             }
         }
-        // Release the wake lock provided by the WakefulBroadcastReceiver.
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
-    private void sendNotification(String msg) {
+    private void sendNotification(Bundle extra) {
         mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, Splash.class), 0);
 
+        String title = extra.getString("title");
+        String message = extra.getString("msg");
+        String photo_url = extra.getString("photo");
+        int NOTIFICATION_ID = Integer.parseInt(extra.getString("notification_id"));
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
-                        .setContentTitle("GCM Notification")
+                        .setContentTitle(title != null ? title : getString(R.string.app_name))
                         .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText(msg);
+                                .bigText(message))
+                        .setAutoCancel(true)
+                        .setContentText(message);
+
+        if (!photo_url.isEmpty()) {
+            ImageLoaderUtil.getInstance().initImageLoader();
+            Bitmap bitmap = ImageLoader.getInstance().loadImageSync(photo_url);
+            mBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                    .bigPicture(bitmap)
+                    .setSummaryText(message)
+                    .setBigContentTitle(title));
+        }
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
-
 }
