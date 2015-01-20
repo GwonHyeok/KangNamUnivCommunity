7ca2908bee10110ab0500d8114a0974aa4feee4b
GwonHyeok <kh4975@naver.com>
2015-01-20 19:05:02 +0900

    Meeting ReplyView Change Like Freeboard

--- app/src/main/java/com/yscn/knucommunity/Activity/MeetingDetailActivity.java
+++ app/src/main/java/com/yscn/knucommunity/Activity/MeetingDetailActivity.java
@@ -1,32 +1,23 @@
 package com.yscn.knucommunity.Activity;
 
-import android.animation.ValueAnimator;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.yscn.knucommunity.CustomView.BaseBoardDetailActivity;
 import com.yscn.knucommunity.CustomView.ClearProgressDialog;
-import com.yscn.knucommunity.Items.CommentListItems;
 import com.yscn.knucommunity.R;
 import com.yscn.knucommunity.Util.NetworkUtil;
 
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Created by GwonHyeok on 14. 11. 5..
@@ -45,7 +36,6 @@ public class MeetingDetailActivity extends BaseBoardDetailActivity implements Vi
         super.onCreate(bundle);
         viewInit();
         setContent();
-        getReplyData();
     }
 
     private void viewInit() {
@@ -62,70 +52,6 @@ public class MeetingDetailActivity extends BaseBoardDetailActivity implements Vi
         titleView.setLayoutParams(layoutParams);
         titleView.setPadding(0, 0, 0, 0);
         titleView.setGravity(Gravity.CENTER);
-    }
-
-    private void getReplyData() {
-        new AsyncTask<Void, Void, ArrayList<CommentListItems>>() {
-            private ClearProgressDialog clearProgressDialog;
-
-            @Override
-            protected void onPreExecute() {
-                clearProgressDialog = new ClearProgressDialog(getContext());
-                clearProgressDialog.show();
-            }
-
-            @Override
-            protected ArrayList<CommentListItems> doInBackground(Void... params) {
-                try {
-                    return NetworkUtil.getInstance().getComment(getIntent().getStringExtra("contentID"));
-                } catch (IOException | ParseException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(ArrayList<CommentListItems> itemses) {
-                if (itemses != null) {
-                    addReplyData(itemses);
-                } else {
-                    /* Error Occured */
-                }
-                clearProgressDialog.cancel();
-            }
-        }.execute();
-    }
-
-    private void addReplyData(ArrayList<CommentListItems> itemses) {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fatdetail_main_scroll_activity);
-
-        for (CommentListItems dataObject : itemses) {
-            View replyView = LayoutInflater.from(this).inflate(R.layout.ui_faqreply, linearLayout, false);
-
-            ImageView profileView = (ImageView) replyView.findViewById(R.id.faq_reply_profile);
-            TextView nameView = (TextView) replyView.findViewById(R.id.faq_reply_name);
-            TextView timeView = (TextView) replyView.findViewById(R.id.faq_reply_time);
-            TextView contentView = (TextView) replyView.findViewById(R.id.faq_reply_content);
-
-            nameView.setText(getReplyNameFormat(dataObject.getName()));
-            timeView.setText(getSimpleDetailTime(dataObject.getTime()));
-            contentView.setText(dataObject.getComment());
-            setProfileImage(profileView, dataObject.getStudentnumber());
-
-            linearLayout.addView(replyView);
-        }
-    }
-
-    private void removeAllReplyData() {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fatdetail_main_scroll_activity);
-        int childCount = linearLayout.getChildCount();
-        if (childCount > 1) {
-            linearLayout.removeViews(1, childCount - 1);
-        }
-    }
-
-    private String getReplyNameFormat(String replyName) {
-        return String.format(getString(R.string.community_faq_reply_who_txt), replyName);
     }
 
     private void setContent() {
@@ -148,7 +74,6 @@ public class MeetingDetailActivity extends BaseBoardDetailActivity implements Vi
                 }
                 return null;
             }
-
 
             @Override
             protected void onPostExecute(String value) {
@@ -197,86 +122,16 @@ public class MeetingDetailActivity extends BaseBoardDetailActivity implements Vi
         return title;
     }
 
-    private String getReplyText(String reply) {
-        return String.format(getString(R.string.community_faq_reply_txt), reply);
-    }
-
     @Override
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.view) {
-            final View replyImageView = findViewById(R.id.imageView);
-            final EditText replyEditText = (EditText) findViewById(R.id.reply_edittext);
-            View replyButtonView = findViewById(R.id.reply_textview);
-
-            int moveX = (int) (replyImageView.getX() + replyImageView.getWidth());
-
-            replyEditText.setCursorVisible(!isReplyMode);
-            replyEditText.setEnabled(!isReplyMode);
-            replyEditText.setSelection(replyEditText.length());
-            replyButtonView.setVisibility(isReplyMode ? View.GONE : View.VISIBLE);
-            replyEditText.setHint(isReplyMode ?
-                    getString(R.string.community_reply_text) : getString(R.string.community_reply_need_text));
-
-            ValueAnimator animation = ValueAnimator.ofFloat(0, -moveX);
-            animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    replyImageView.setTranslationX((float) animation.getAnimatedValue());
-                    replyEditText.setTranslationX((float) animation.getAnimatedValue());
-                }
-            });
-
-            if (!isReplyMode) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                        inputMethodManager.showSoftInput(replyEditText, InputMethodManager.SHOW_IMPLICIT);
-                    }
-                }, 400);
-            }
-
-            animation.setDuration(400);
-            animation.start();
-
-            isReplyMode = !isReplyMode;
-
-        } else if (id == R.id.reply_textview) {
-            addComment();
-            onClick(findViewById(R.id.view));
+            // 자유개시판이랑 같아서 우선 이용
+            TextView titleView = ((TextView) findViewById(R.id.faq_detail_title));
+            Intent intent = new Intent(this, FreeBoardReplyActivity.class);
+            intent.putExtra("contentID", getIntent().getStringExtra("contentID"));
+            intent.putExtra("title", titleView.getText().toString());
+            startActivity(intent);
         }
-    }
-
-    private void addComment() {
-        final String comment = ((EditText) findViewById(R.id.reply_edittext)).getText().toString();
-
-        if (comment.isEmpty()) {
-            return;
-        }
-
-        new AsyncTask<Void, Void, Boolean>() {
-
-            @Override
-            protected Boolean doInBackground(Void... params) {
-                boolean result = false;
-                try {
-                    result = NetworkUtil.getInstance().writeComment(
-                            getIntent().getStringExtra("contentID"), comment);
-                } catch (IOException | ParseException e) {
-                    e.printStackTrace();
-                }
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(Boolean result) {
-                if (result) {
-                    ((EditText) findViewById(R.id.reply_edittext)).setText("");
-                    removeAllReplyData();
-                    getReplyData();
-                }
-            }
-        }.execute();
     }
 }
