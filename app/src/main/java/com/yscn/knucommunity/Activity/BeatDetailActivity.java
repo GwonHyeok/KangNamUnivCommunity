fcdd265a56fdc5ad83fb8aa7cf5607dc6f459226
GwonHyeok <kh4975@naver.com>
2015-02-19 17:44:58 +0900

    add beatdetaili use available attatchment

--- app/src/main/java/com/yscn/knucommunity/Activity/BeatDetailActivity.java
+++ app/src/main/java/com/yscn/knucommunity/Activity/BeatDetailActivity.java
@@ -1,17 +1,30 @@
 package com.yscn.knucommunity.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.yscn.knucommunity.R;
+import com.yscn.knucommunity.Util.ApplicationUtil;
+import com.yscn.knucommunity.Util.ImageLoaderUtil;
 import com.yscn.knucommunity.Util.NetworkUtil;
+import com.yscn.knucommunity.Util.UrlList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
@@ -29,8 +42,23 @@ public class BeatDetailActivity extends ActionBarActivity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_beat_detail);
+        toolbarInit();
         setBeatIntentData();
         setBeatContentData();
+        ApplicationUtil.getInstance().setTypeFace(findViewById(R.id.beat_detail_root));
+    }
+
+    private void toolbarInit() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        mToolbar.setNavigationIcon(R.drawable.ic_cancel);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void setBeatContentData() {
@@ -59,8 +87,17 @@ public class BeatDetailActivity extends ActionBarActivity {
             @Override
             protected void onPostExecute(JSONObject jsonObject) {
                 mProgressbar.setVisibility(View.GONE);
+                invalidateOptionsMenu();
                 Log.d(getClass().getSimpleName(), jsonObject.toJSONString());
-//                {"result":"success","data":[{"studentnumber":null,"title":"문화 누리카드 특별할인","beatid":"1","time":"2015-02-18 17:23:00","content":"이 달의 룩엔룩 최고의 좋아요를 기록한 학생은 컴미공의 14학번 꼬녁 학생이 차지했습니다. 그럼 그 학생의 아름다운 패션을 볼까요?!","id":"5"}]}
+                JSONArray attatchmentArray = (JSONArray) jsonObject.get("attachment");
+
+                String[] attatchmenturls = new String[attatchmentArray.size()];
+                for (int i = 0; i < attatchmentArray.size(); i++) {
+                    JSONObject attatchmentObject = (JSONObject) attatchmentArray.get(i);
+                    attatchmenturls[i] = attatchmentObject.get("filename").toString();
+                }
+                addPhotoView(attatchmenturls);
+
                 try {
                     ((TextView) findViewById(R.id.beat_detail_title)).setText(jsonObject.get("title").toString());
                     ((TextView) findViewById(R.id.beat_detail_content)).setText(jsonObject.get("content").toString());
@@ -72,9 +109,65 @@ public class BeatDetailActivity extends ActionBarActivity {
         }.execute();
     }
 
+    private void addPhotoView(String[] urls) {
+        ImageLoaderUtil.getInstance().initImageLoader();
+        LinearLayout imageGroup = (LinearLayout) findViewById(R.id.beat_detail_scrollview_imagegroup);
+
+        for (String url : urls) {
+            View view = getLayoutInflater().inflate(R.layout.ui_board_image_card, imageGroup, false);
+            final ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
+            ImageLoader.getInstance().displayImage(UrlList.MAIN_URL + url, imageView,
+                    ImageLoaderUtil.getInstance().getNoCacheImageOptions(), new ImageLoadingListener() {
+                        @Override
+                        public void onLoadingStarted(String imageUri, View view) {
+                            progressBar.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onLoadingCancelled(String imageUri, View view) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+            imageGroup.addView(view);
+        }
+    }
+
     private void setBeatIntentData() {
         Intent intent = getIntent();
         mBeatIndex = intent.getIntExtra("beatindex", -1);
         mContentId = intent.getStringExtra("contentid");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mBeatIndex == 3) {
+            getMenuInflater().inflate(R.menu.board_detail_menu, menu);
+            menu.getItem(1).setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_trash) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
