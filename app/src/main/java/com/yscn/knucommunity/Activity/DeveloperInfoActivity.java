46a271e1727cf7b7e2cf91596ff63282ca4fce7a
GwonHyeok <kh4975@naver.com>
2015-02-20 01:39:52 +0900

    Developer info add fb url

--- app/src/main/java/com/yscn/knucommunity/Activity/DeveloperInfoActivity.java
+++ app/src/main/java/com/yscn/knucommunity/Activity/DeveloperInfoActivity.java
@@ -1,12 +1,16 @@
 package com.yscn.knucommunity.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.yscn.knucommunity.R;
+import com.yscn.knucommunity.Util.ApplicationUtil;
 
 /**
  * Created by GwonHyeok on 15. 2. 19..
@@ -22,5 +26,24 @@ public class DeveloperInfoActivity extends ActionBarActivity {
         if (Build.VERSION.SDK_INT >= 19) {
             w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+        ApplicationUtil.getInstance().setTypeFace(findViewById(R.id.developer_info_root));
+    }
+
+    public void onClickLeeFB(View view) {
+        openWebPage("http://www.facebook.com/mmimics");
+    }
+
+    public void onClickMhwanFB(View view) {
+        openWebPage("http://www.facebook.com/mhwanbae21");
+    }
+
+    public void onClickHyeokFB(View view) {
+        openWebPage("http://facebook.com/Hyeok.G");
+    }
+
+    public void openWebPage(String URL) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(URL));
+        startActivity(intent);
     }
 }
