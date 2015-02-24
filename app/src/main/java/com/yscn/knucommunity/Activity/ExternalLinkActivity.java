37ee4b46ef533fb853ea7e0800dae4bb42ac21eb
GwonHyeok <kh4975@naver.com>
2015-02-25 00:56:03 +0900

    external Link Activity change onClick

--- app/src/main/java/com/yscn/knucommunity/Activity/ExternalLinkActivity.java
+++ app/src/main/java/com/yscn/knucommunity/Activity/ExternalLinkActivity.java
@@ -16,7 +16,7 @@ import com.yscn.knucommunity.Util.ApplicationUtil;
 /**
  * Created by GwonHyeok on 15. 2. 22..
  */
-public class ExternalLinkActivity extends ActionBarActivity {
+public class ExternalLinkActivity extends ActionBarActivity implements View.OnClickListener {
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -40,23 +40,30 @@ public class ExternalLinkActivity extends ActionBarActivity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
+        findViewById(R.id.external_link_externalscholarInfo).setOnClickListener(this);
+        findViewById(R.id.external_link_contestInfo).setOnClickListener(this);
+        findViewById(R.id.external_link_externalactivityInfo).setOnClickListener(this);
+
         ApplicationUtil.getInstance().setTypeFace(findViewById(R.id.external_link_root));
-    }
-
-    public void externalscholarInfo(View view) {
-        startWeb("http://mbanote2.tistory.com/37");
-    }
-
-    public void contestInfo(View view) {
-        startWeb("http://contests.saramin.co.kr/contests");
-    }
-
-    public void externalactivityInfo(View view) {
-        startWeb("http://contests.saramin.co.kr/outreach");
     }
 
     private void startWeb(String url) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.external_link_externalscholarInfo:
+                startWeb("http://mbanote2.tistory.com/37");
+                break;
+            case R.id.external_link_contestInfo:
+                startWeb("http://contests.saramin.co.kr/contests");
+                break;
+            case R.id.external_link_externalactivityInfo:
+                startWeb("http://contests.saramin.co.kr/outreach");
+                break;
+        }
     }
 }
